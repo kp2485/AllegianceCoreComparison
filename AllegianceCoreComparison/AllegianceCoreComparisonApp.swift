36b2851efdb46c9d937b5b7fcbfa633288f01b32
Kyle Peterson <kdpeters@gmail.com>
2024-11-02 13:37:58 -0400

    Initial Commit

---
+++ AllegianceCoreComparison/AllegianceCoreComparisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllegianceCoreComparisonApp.swift
+//  AllegianceCoreComparison
+//
+//  Created by Kyle Peterson on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AllegianceCoreComparisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllegianceCoreComparisonTests/AllegianceCoreComparisonTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllegianceCoreComparisonTests.swift
+//  AllegianceCoreComparisonTests
+//
+//  Created by Kyle Peterson on 11/2/24.
+//
+
+import Testing
+@testable import AllegianceCoreComparison
+
+struct AllegianceCoreComparisonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
