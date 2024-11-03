75cfb1f27f14c25b27410dcfab12dba6677ab03c
Kyle Peterson <kdpeters@gmail.com>
2024-11-02 23:26:33 -0400

    fixed cloaks

--- AllegianceCoreComparison/Models/Cloak.swift
+++ AllegianceCoreComparison/Models/Cloak.swift
@@ -9,25 +9,67 @@ import Foundation
 
 struct Cloak: Identifiable, CSVParsable {
     let id: Int
-    let name: String
     let energyConsumption: Double
-    let effectiveness: Double
-    let cooldown: Double
+    let maxCloaking: Double
+    let powerOnRate: Double
+    let powerOffRate: Double
+    let engageSfxID: Int
+    let disengageSfxID: Int
+    let mass: Int
+    let signature: Double
+    let successorPartID: Int
+    let partMask: Int
+    let inventoryModel: String
+    let price: Int
+    let timeToBuild: Int
+    let modelName: String
+    let iconName: String
+    let name: String
     let description: String
+    let group: Int
+    let techRequired: String
+    let techEffect: String
 
     init?(csvRow: [String]) {
-        guard csvRow.count >= 6,
+        guard csvRow.count >= 21,
               let id = Int(csvRow[0]),
-              let energyConsumption = Double(csvRow[2]),
-              let effectiveness = Double(csvRow[3]),
-              let cooldown = Double(csvRow[4]) else {
+              let energyConsumption = Double(csvRow[1]),
+              let maxCloaking = Double(csvRow[2]),
+              let powerOnRate = Double(csvRow[3]),
+              let powerOffRate = Double(csvRow[4]),
+              let engageSfxID = Int(csvRow[5]),
+              let disengageSfxID = Int(csvRow[6]),
+              let mass = Int(csvRow[7]),
+              let signature = Double(csvRow[8]),
+              let successorPartID = Int(csvRow[9]),
+              let partMask = Int(csvRow[10]),
+              let price = Int(csvRow[12]),
+              let timeToBuild = Int(csvRow[13]),
+              let group = Int(csvRow[18]) else {
             return nil
         }
+
         self.id = id
-        self.name = csvRow[1]
         self.energyConsumption = energyConsumption
-        self.effectiveness = effectiveness
-        self.cooldown = cooldown
-        self.description = csvRow[5]
+        self.maxCloaking = maxCloaking
+        self.powerOnRate = powerOnRate
+        self.powerOffRate = powerOffRate
+        self.engageSfxID = engageSfxID
+        self.disengageSfxID = disengageSfxID
+        self.mass = mass
+        self.signature = signature
+        self.successorPartID = successorPartID
+        self.partMask = partMask
+        self.inventoryModel = csvRow[11]
+        self.price = price
+        self.timeToBuild = timeToBuild
+        self.modelName = csvRow[14]
+        self.iconName = csvRow[15]
+        self.name = csvRow[16]
+        self.description = csvRow[17]
+        self.group = group
+        self.techRequired = csvRow[19]
+        self.techEffect = csvRow[20]
     }
 }
+
--- AllegianceCoreComparison/Views/CloakListView.swift
+++ AllegianceCoreComparison/Views/CloakListView.swift
@@ -12,17 +12,32 @@ struct CloakListView: View {
 
     var body: some View {
         List(cloaks) { cloak in
-            VStack(alignment: .leading) {
-                Text(cloak.name).font(.headline)
+            VStack(alignment: .leading, spacing: 5) {
+                Text("Name: \(cloak.name)").font(.headline)
+                Text("ID: \(cloak.id)")
                 Text("Energy Consumption: \(cloak.energyConsumption)")
-                Text("Effectiveness: \(cloak.effectiveness)")
-                Text("Cooldown: \(cloak.cooldown)")
+                Text("Max Cloaking: \(cloak.maxCloaking)")
+                Text("Power On Rate: \(cloak.powerOnRate)")
+                Text("Power Off Rate: \(cloak.powerOffRate)")
+                Text("Engage SFX ID: \(cloak.engageSfxID)")
+                Text("Disengage SFX ID: \(cloak.disengageSfxID)")
+                Text("Mass: \(cloak.mass)")
+                Text("Signature: \(cloak.signature)")
+                Text("Successor Part ID: \(cloak.successorPartID)")
+                Text("Part Mask: \(cloak.partMask)")
+                Text("Inventory Model: \(cloak.inventoryModel)")
+                Text("Price: \(cloak.price)")
+                Text("Time to Build: \(cloak.timeToBuild)")
+                Text("Model Name: \(cloak.modelName)")
+                Text("Icon Name: \(cloak.iconName)")
                 Text("Description: \(cloak.description)")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
+                Text("Group: \(cloak.group)")
+                Text("Tech Required: \(cloak.techRequired)")
+                Text("Tech Effect: \(cloak.techEffect)")
             }
             .padding(.vertical, 5)
         }
         .navigationTitle("Cloaks")
     }
 }
+
