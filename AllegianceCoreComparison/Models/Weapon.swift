bb0355c02426a7fcdb1bec40c47082c22cf120d8
Kyle Peterson <kdpeters@gmail.com>
2024-11-02 23:13:39 -0400

    fixed weapons

--- AllegianceCoreComparison/Models/Weapon.swift
+++ AllegianceCoreComparison/Models/Weapon.swift
@@ -9,25 +9,75 @@ import Foundation
 
 struct Weapon: Identifiable, CSVParsable {
     let id: Int
+    let readyTime: Double
+    let burstTime: Double
+    let energyPerShot: Double
+    let dispersion: Double
+    let ammoPerShot: Int
+    let projectileTypeID: Int
+    let activateSfxID: Int
+    let shotSfxID: Int
+    let burstSfxID: Int
+    let mass: Double
+    let signature: Double
+    let successorPartID: Int
+    let partMask: Int
+    let inventoryModel: String
+    let price: Int
+    let timeToBuild: Double
+    let modelName: String
+    let iconName: String
     let name: String
-    let damage: Double
-    let range: Double
-    let fireRate: Double
     let description: String
+    let group: Int
+    let techRequired: String
+    let techEffect: String
 
     init?(csvRow: [String]) {
-        guard csvRow.count >= 6,
+        guard csvRow.count >= 24,
               let id = Int(csvRow[0]),
-              let damage = Double(csvRow[2]),
-              let range = Double(csvRow[3]),
-              let fireRate = Double(csvRow[4]) else {
+              let readyTime = Double(csvRow[1]),
+              let burstTime = Double(csvRow[2]),
+              let energyPerShot = Double(csvRow[3]),
+              let dispersion = Double(csvRow[4]),
+              let ammoPerShot = Int(csvRow[5]),
+              let projectileTypeID = Int(csvRow[6]),
+              let activateSfxID = Int(csvRow[7]),
+              let shotSfxID = Int(csvRow[8]),
+              let burstSfxID = Int(csvRow[9]),
+              let mass = Double(csvRow[10]),
+              let signature = Double(csvRow[11]),
+              let successorPartID = Int(csvRow[12]),
+              let partMask = Int(csvRow[13]),
+              let price = Int(csvRow[15]),
+              let timeToBuild = Double(csvRow[16]) else {
             return nil
         }
+
         self.id = id
-        self.name = csvRow[1]
-        self.damage = damage
-        self.range = range
-        self.fireRate = fireRate
-        self.description = csvRow[5]
+        self.readyTime = readyTime
+        self.burstTime = burstTime
+        self.energyPerShot = energyPerShot
+        self.dispersion = dispersion
+        self.ammoPerShot = ammoPerShot
+        self.projectileTypeID = projectileTypeID
+        self.activateSfxID = activateSfxID
+        self.shotSfxID = shotSfxID
+        self.burstSfxID = burstSfxID
+        self.mass = mass
+        self.signature = signature
+        self.successorPartID = successorPartID
+        self.partMask = partMask
+        self.inventoryModel = csvRow[14]
+        self.price = price
+        self.timeToBuild = timeToBuild
+        self.modelName = csvRow[17]
+        self.iconName = csvRow[18]
+        self.name = csvRow[19]
+        self.description = csvRow[20]
+        self.group = Int(csvRow[21]) ?? 0
+        self.techRequired = csvRow[22]
+        self.techEffect = csvRow[23]
     }
 }
+
--- AllegianceCoreComparison/Views/WeaponListView.swift
+++ AllegianceCoreComparison/Views/WeaponListView.swift
@@ -12,14 +12,31 @@ struct WeaponListView: View {
 
     var body: some View {
         List(weapons) { weapon in
-            VStack(alignment: .leading) {
-                Text(weapon.name).font(.headline)
-                Text("Damage: \(weapon.damage)")
-                Text("Range: \(weapon.range)")
-                Text("Fire Rate: \(weapon.fireRate)")
+            VStack(alignment: .leading, spacing: 5) {
+                Text("Name: \(weapon.name)").font(.headline)
+                Text("ID: \(weapon.id)")
+                Text("Ready Time: \(weapon.readyTime)")
+                Text("Burst Time: \(weapon.burstTime)")
+                Text("Energy per Shot: \(weapon.energyPerShot)")
+                Text("Dispersion: \(weapon.dispersion)")
+                Text("Ammo per Shot: \(weapon.ammoPerShot)")
+                Text("Projectile Type ID: \(weapon.projectileTypeID)")
+                Text("Activate SFX ID: \(weapon.activateSfxID)")
+                Text("Shot SFX ID: \(weapon.shotSfxID)")
+                Text("Burst SFX ID: \(weapon.burstSfxID)")
+                Text("Mass: \(weapon.mass)")
+                Text("Signature: \(weapon.signature)")
+                Text("Successor Part ID: \(weapon.successorPartID)")
+                Text("Part Mask: \(weapon.partMask)")
+                Text("Inventory Model: \(weapon.inventoryModel)")
+                Text("Price: \(weapon.price)")
+                Text("Time to Build: \(weapon.timeToBuild)")
+                Text("Model Name: \(weapon.modelName)")
+                Text("Icon Name: \(weapon.iconName)")
                 Text("Description: \(weapon.description)")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
+                Text("Group: \(weapon.group)")
+                Text("Tech Required: \(weapon.techRequired)")
+                Text("Tech Effect: \(weapon.techEffect)")
             }
             .padding(.vertical, 5)
         }
